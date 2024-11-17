97a5b1d092d7246834e59f5d2c3df16e69c40488
Kumao-Y-I <denngeki8117@live.jp>
2024-11-17 17:46:24 +0900

    comit1

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import {createRoot} from "react-dom/client"
 
-createRoot(document.getElementById('root')).render(
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+
+const App = () => {
+  return  <h1>こんにちは！</h1>
+}
+
+root.render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <App>こんにちは！</App>
+  </StrictMode>
+);
